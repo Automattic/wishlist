1324f6c02aba203babb94774f1c780aa8eb675c9
Welly Shen <welly.shen@automattic.com>
2025-04-02 14:32:47 +0200

    Implement UIs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 export default function Home() {
 	return (
-		<main>
-			<h1>GIFTER</h1>
-			<p>Get 9 gift recommendations for a friends</p>
-			<form>
-				<label htmlFor="email">Enter your email</label>
-				<input id="email" placeholder="Enter your friend’s email" />
-				<label htmlFor="budget">How much would you like to spend?</label>
-				<input id="budget" placeholder="Not a lot ($5 — $30)" />
-				<input type="submit" />
+		<main className="flex flex-col items-center text-center">
+			<h1 className="text-black font-sans text-[44px] font-semibold leading-[1.2]">
+				GIFTER
+			</h1>
+			<p className="text-black font-sans text-[24px] font-semibold leading-[1.2]">Get 9 gift recommendations for a friends</p>
+			<form className="w-full">
+				<div>
+					<label htmlFor="email" className="hidden">Enter your email</label>
+					<input id="email" className="w-full border border-[#B3B3B3] h-[63px] p-[10px]" placeholder="Enter your friend’s email" required />
+				</div>
+				<div>
+					<label htmlFor="name">Enter your friend’s name</label>
+					<input id="name" className="w-full border border-[#B3B3B3] h-[63px] p-[10px]" placeholder="Enter your friend’s name" required />
+				</div>
+				<button type="submit">Get started</button>
 			</form>
 			<small>By continuing you agree to our terms</small>
 		</main>
