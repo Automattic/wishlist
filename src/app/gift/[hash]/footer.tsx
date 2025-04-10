69705a223f23f0d0894d5a74f8274e502514417f
Joao Heringer <joao.heringer@automattic.com>
2025-04-10 17:12:23 -0300

    Extract components

---
+++ src/app/gift/[hash]/footer.tsx
@@ -0,0 +1,69 @@
+import Image from 'next/image';
+import { DbProduct } from '@/products/types';
+
+type FooterProps = {
+	selectedProducts: DbProduct[];
+	onSeeWishlist: () => void;
+}
+
+export const Footer = ( { selectedProducts, onSeeWishlist }: FooterProps ) => {
+	return (
+		<div className="h-[72px] flex items-center justify-center shrink-0 gap-2 mt-6">
+			{ selectedProducts.length === 0 ? (
+				<>
+					<span className="max-w-[170px] text-center">Swipe any card right to add it to your wishlist</span>
+					<Image
+						src="/images/styled-arrow-right.svg"
+						className="w-[24px] h-[17px]"
+						width="24"
+						height="17"
+						alt=""
+					/>
+				</>
+			) : (
+				<>
+					<div className="w-full flex">
+						{ selectedProducts.slice( 0, 3 ).map( ( product, index ) => {
+							return (
+								<div
+									key={ product.id }
+									className="w-[72px] h-[72px] rounded-full overflow-hidden bg-white border boder-black"
+									style={ index ? { marginLeft: '-24px' } : undefined }
+								>
+									<Image
+										src={ product.imageUrl }
+										className="w-[72px] h-[72px] object-cover"
+										width={ 72 }
+										height={ 72 }
+										alt=""
+									/>
+								</div>
+							);
+						} ) }
+						{ selectedProducts.length > 3 && (
+							<div
+								className="w-[72px] h-[72px] rounded-full overflow-hidden bg-white border border-black flex items-center justify-center"
+								style={ { marginLeft: '-24px' } }
+							>
+								<span className="text-xl font-bold">+{ selectedProducts.length - 3 }</span>
+							</div>
+						) }
+					</div>
+					<button
+						className="flex items-center gap-1 shrink-0 cursor-pointer"
+						onClick={ onSeeWishlist }
+					>
+						<span>See wishlist</span>
+						<Image
+							src="/icons/ico-arrow-right.svg"
+							className="w-[24px] h-[24px]"
+							width={ 24 }
+							height={ 24 }
+							alt=""
+						/>
+					</button>
+				</>
+			) }
+		</div>
+	);
+}; 
---
+++ src/app/gift/[hash]/wishlist.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import { User } from '@/types';
+import { DbProduct } from '@/products/types';
+
+type WishlistProps = {
+	selectedProducts: DbProduct[];
+	userData: User;
+	currentCardIndex: number;
+}
+
+export const Wishlist = ( {
+	selectedProducts,
+	userData,
+	currentCardIndex,
+}: WishlistProps ) => {
+	const firstName = userData.display_name.split( ' ' )[ 0 ];
+
+	return (
+		<div className="flex flex-col h-full">
+			<h2 className="text-black text-[32px] font-bold mt-3 mb-2">
+				These are your { selectedProducts.length } picks for { firstName }
+			</h2>
+			<p className="text-black text-xl mb-8">
+				It only took us { currentCardIndex } products to get here, good job.
+			</p>
+			<div className="flex-1 overflow-auto">
+				<div className="grid grid-cols-3 gap-6 max-w-[348px] mx-auto">
+					{ selectedProducts.map( ( product ) => (
+						<div
+							key={ product.id }
+							className="aspect-square w-full max-w-[100px] rounded-full overflow-hidden bg-white border border-black mx-auto"
+						>
+							<Image
+								src={ product.imageUrl }
+								className="w-full h-full object-cover"
+								width={ 100 }
+								height={ 100 }
+								alt={ product.productName }
+							/>
+						</div>
+					) ) }
+				</div>
+			</div>
+		</div>
+	);
+}; 
